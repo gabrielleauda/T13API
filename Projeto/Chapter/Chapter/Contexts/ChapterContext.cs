93a7f1ee1fdd3e33066057fbc8065b86e179b7b0
gabrielleauda <gabi_auda@hotmail.com>
2023-04-14 18:14:34 -0300

    entrega 1

---
+++ Projeto/Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,28 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+
+        }
+
+        public ChapterContext (DbContextOptions<ChapterContext> options) : base(options) 
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Soucer = DESKTOP-IIV620H\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+
+        public DbSet<Livros> Livros { get; set; }
+    }
+}
---
+++ Projeto/Chapter/Chapter/Models/Livros.cs
@@ -0,0 +1,11 @@
+﻿namespace Chapter.Models
+{
+    public class Livros
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set;}
+        public int QuantidadePaginas { get; set; }
+        public int Disponnivel { get; set; }
+
+    }
+}
